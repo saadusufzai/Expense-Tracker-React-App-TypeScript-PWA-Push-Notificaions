bea35b1e39360650996e584a9717d738a5076227
Saad Aslam <saadusufzai@gmail.com>
2020-09-10 22:14:30 +0500

    Somewhere

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Header } from './Components/Header';
+import { Balance } from './Components/Balance';
+import { IncomeExpenses } from './Components/IncomeExpenses';
+import { TransactionList } from './Components/TransactionList';
+import { AddTransaction } from './Components/AddTransaction';
+
+import { GlobalProvider } from './context/GlobalState';
+
 import './App.css';
 
 function App() {
   return (
-    <div>APP</div>
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
   );
 }
 
---
+++ src/Components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <div>
+            <h2>
+                Expense Tracker
+            </h2>
+        </div>
+    )
+}
